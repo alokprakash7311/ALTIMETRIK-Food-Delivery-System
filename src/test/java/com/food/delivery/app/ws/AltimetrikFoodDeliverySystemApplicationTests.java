9cc9c7b7198326cba144867b0da59655958c3df3
alokprakash7311 <69879007+alokprakash7311@users.noreply.github.com>
2021-07-20 00:17:36 +0530

    Update AltimetrikFoodDeliverySystemApplicationTests.java

--- src/test/java/com/food/delivery/app/ws/AltimetrikFoodDeliverySystemApplicationTests.java
+++ src/test/java/com/food/delivery/app/ws/AltimetrikFoodDeliverySystemApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class AltimetrikFoodDeliverySystemApplicationTests {
+public class AltimetrikFoodDeliverySystemApplicationTests {
 
 	@Test
 	void contextLoads() {
